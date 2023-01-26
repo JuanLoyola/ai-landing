8b860d340352e767d8fd07933dc2bb4fa3a13a25
Loyola <juanjoloyola@hotmail.com>
2023-01-26 19:03:31 -0300

    core component added

--- src/components/home/Cards.vue
+++ src/components/home/Cards.vue
@@ -6,25 +6,25 @@
                     class="image md:h-full flex-grow flex flex-col justify-between bg-cover bg-[url('https://images.unsplash.com/photo-1601597110547-78516f198ce4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')]"
                 >
                     <h1 class="text-white font-bold text-sm pt-5" style="text-shadow: 1px 1px 2px black;">Banking</h1>
-                    <button class="btn bg-indigo-300  px-5 py-2 rounded text-white">See more</button>
+                    <button class="btn relative inline-flex items-center rounded-t-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800">See more</button>
                 </div>
                 <div
                     class="image md:h-full flex-grow flex flex-col justify-between bg-cover bg-[url('https://images.unsplash.com/photo-1594665964721-d96c7e10981c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')]"
                 >
                     <h1 class="text-white font-bold text-sm pt-5" style="text-shadow: 1px 1px 2px black;">Insurance</h1>
-                    <button class="btn bg-indigo-300  px-5 py-2 rounded text-white">See more</button>
+                    <button class="btn relative inline-flex items-center rounded-t-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800">See more</button>
                 </div>
                 <div
                     class="image md:h-full flex-grow flex flex-col justify-between bg-cover bg-[url('https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')]"
                 >
                     <h1 class="text-white font-bold text-sm pt-5" style="text-shadow: 1px 1px 2px black;">Education</h1>
-                    <button class="btn bg-indigo-300  px-5 py-2 rounded text-white">See more</button>
+                    <button class="btn relative inline-flex items-center rounded-t-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800">See more</button>
                 </div>
                 <div
                     class="image md:h-full flex-grow flex flex-col justify-between bg-cover bg-[url('https://images.unsplash.com/photo-1601598851547-4302969d0614?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmV0YWlsfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60')]"
                 >
                     <h1 class="text-white font-bold text-sm pt-5" style="text-shadow: 1px 1px 2px black;">Retail & FMCG</h1>
-                    <button class="btn bg-indigo-300  px-5 py-2 rounded text-white">See more</button>
+                    <button class="btn relative inline-flex items-center rounded-t-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800">See more</button>
                 </div>
                 <div
                     class="image md:h-full flex-grow flex flex-col justify-between bg-cover bg-[url('https://images.unsplash.com/photo-1590098563176-07884b06d7f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')]"
@@ -33,7 +33,7 @@
                     <p class="hidden hover:block">
                         Some of the world’s largest Wealth Management & Financial Advisory firms rely on Fligoo Sharp AI™ to understand their customers.
                     </p>
-                    <button class="btn bg-indigo-300  px-5 py-2 rounded text-white">See more</button>
+                    <button class="btn relative inline-flex items-center rounded-t-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800">See more</button>
                 </div>
             </section>
         </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <Hero />
     <BannerInfo />
     <Cards />
+    <Core />
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import Hero from '@/components/home/Hero.vue';
 import BannerInfo from '@/components/home/BannerInfo.vue';
 import Cards from '@/components/home/Cards.vue';
+import Core from '@/components/home/Core.vue';
 
 export default {
   name: 'HomeView',
   components: {
     Hero,
     BannerInfo,
-    Cards
+    Cards,
+    Core
   }
 }
 </script>
