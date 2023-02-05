0f2062cbdb860e72dc314285054fce3e35fb6952
Juan Loyola <juanjoloyola@hotmail.com>
2023-02-05 13:28:47 -0300

    Overflow fixed on animations

--- src/components/home/InfoCard.vue
+++ src/components/home/InfoCard.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-full bg-[#252F3E]">
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8 h-full md:h-screen flex  flex-col justify-around items-center md:items-center">
+    <div class="w-full bg-[#252F3E] overflow-x-hidden">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8 h-full flex  flex-col justify-around items-center md:items-center">
 
             <div class="flex flex-col md:flex-row gap-y-5 md:gap-y-0  md:gap-x-3 ">
                 <Card
