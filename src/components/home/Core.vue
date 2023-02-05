37a3797184236680bd4bb292ee43119f4da12267
Juan Loyola <juanjoloyola@hotmail.com>
2023-02-05 13:43:12 -0300

    margin top fixed

--- src/components/home/Core.vue
+++ src/components/home/Core.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full bg-[#252F3E]">
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8 h-screen md:h-[100vh] flex flex-col justify-center items-center md:items-center">
-            <h1 data-aos="fade-down" data-aos-duration="2000" class="text-center font-bold text-xl lg:text-2xl text-gray-500 ">Core technology™</h1>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 h-screen flex flex-col justify-center items-center md:items-center">
+            <h1 data-aos="fade-down" data-aos-duration="2000" class="mt-12 text-center font-bold text-xl lg:text-2xl text-gray-500 ">Core technology™</h1>
             <h2 data-aos="fade-down" data-aos-duration="2000" class="mt-5 mb-14 font-medium text-xl lg:text-5xl text-white ">Our proprietary technology, Lorem, ipsum AI™, lets your company work smarter.</h2>
             <h1 data-aos="fade-down" data-aos-duration="2500" class="text-center font-bold text-xl lg:text-xl text-gray-500 ">Lorem, ipsum AI™ integrates with your existing data sources to centralize <br class="hidden md:block"> your data in a big data environment.</h1>
 
